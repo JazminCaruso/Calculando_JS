3d435b8dc61d0a133870d80991889fab17dd8823
JazminCaruso <carusojazmin@gmail.com>
2022-06-21 22:36:09 -0300

    ecuaciones desde js

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,34 +1,34 @@
-let historial = []
+// let historial = []
 
-function ecSegundoGrado(a, b, c) {
-    if ((b**2 - 4*a*c) < 0) {
-        alert("El discriminante es menor que 0");
-    } else if (b > 0) {
-        x_1 = (-b - ((b**2 - 4*a*c)**0.5)) / (2.0 * a);
-        x_2 = (c / a) / x_1;
-        let resultado = [x_1, x_2];
-        alert("Las raíces de la función son: " + resultado);
-        historial.push(resultado)
-    } else {
-        x_1 = (-b + ((b**2 - 4*a*c)**0.5)) / (2.0 * a);
-        x_2 = (c / a) / x_1;
-        let resultado = [x_1, x_2];
-        alert("Las raíces de la función son: " + resultado);
-        historial.push(resultado)
-    }
-}
+// function ecSegundoGrado(a, b, c) {
+//     if ((b**2 - 4*a*c) < 0) {
+//         alert("El discriminante es menor que 0");
+//     } else if (b > 0) {
+//         x_1 = (-b - ((b**2 - 4*a*c)**0.5)) / (2.0 * a);
+//         x_2 = (c / a) / x_1;
+//         let resultado = [x_1, x_2];
+//         alert("Las raíces de la función son: " + resultado);
+//         historial.push(resultado)
+//     } else {
+//         x_1 = (-b + ((b**2 - 4*a*c)**0.5)) / (2.0 * a);
+//         x_2 = (c / a) / x_1;
+//         let resultado = [x_1, x_2];
+//         alert("Las raíces de la función son: " + resultado);
+//         historial.push(resultado)
+//     }
+// }
 
-alert("La siguiente calculadora reseulve ecuaciones de segundo grado.\nEstas son del tipo ax**2 + bx + c.")
-let cond = prompt("Ingrese la palabra calcular si quiere calcular las raíces de una ecuación.\nSi desea salir, escriba otra cosa.")
-while (cond == "calcular") {
-    alert("Por favor, a continuación ingrese los datos requeridos.\nPrimero el valor de a, luego el de b, y por último el de c.")
-    let a = parseInt(prompt("Ingrese el coeficiente cuadrático, a"))
-    let b = parseInt(prompt("Ingrese el coeficiente lineal, b"))
-    let c = parseInt(prompt("Ingrese el término independiente, c"))
-    ecSegundoGrado(a, b, c)
-    alert("El historial de soluciones es: " + historial)
-    cond = prompt("Ingrese la palabra calcular si quiere realizar otro cálculo.\nSi desea salir, escriba otra cosa.")
-}
+// alert("La siguiente calculadora reseulve ecuaciones de segundo grado.\nEstas son del tipo ax**2 + bx + c.")
+// let cond = prompt("Ingrese la palabra calcular si quiere calcular las raíces de una ecuación.\nSi desea salir, escriba otra cosa.")
+// while (cond == "calcular") {
+//     alert("Por favor, a continuación ingrese los datos requeridos.\nPrimero el valor de a, luego el de b, y por último el de c.")
+//     let a = parseInt(prompt("Ingrese el coeficiente cuadrático, a"))
+//     let b = parseInt(prompt("Ingrese el coeficiente lineal, b"))
+//     let c = parseInt(prompt("Ingrese el término independiente, c"))
+//     ecSegundoGrado(a, b, c)
+//     alert("El historial de soluciones es: " + historial)
+//     cond = prompt("Ingrese la palabra calcular si quiere realizar otro cálculo.\nSi desea salir, escriba otra cosa.")
+// }
 
 // -----------------listas-----------------
 
