fd041fc388311066f119ba6aa542305e448ca9bd
JazminCaruso <carusojazmin@gmail.com>
2022-06-24 00:56:28 -0300

    input

--- javaScript/ecuaciones.js
+++ javaScript/ecuaciones.js
@@ -1,9 +1,11 @@
-// ----------------------- ecuación primer grado -----------------------
+// este código funciona pero no como quiero
+// lo que quiero es que los valores sean tomados de los inputs que están dentro del cuadro gris
+// ya sé que en este código hago referencia a los inputs del form pero 
+// es la única manera que se pueden tomar y trabajar los valores
+// en el código que está abajo comentado intento tomar los valores de los inputs del cuadro gris pero no funciona
 
-let coefLineal = document.getElementById("coefLineal").value
-let a = parseInt(coefLineal)
-let terminoInd = document.getElementById("terminoInd").value
-let b = parseInt(terminoInd)
+let formulario = document.getElementById("formulario")
+formulario.addEventListener("submit", validar)
 
 let operacion = 0
 let resultado = 0
@@ -12,85 +14,127 @@ function ecPrimerGrado(a, b) {
         resultado = "inexistente"
     } else {
         operacion = (-b) / a
-        resultado = operacion.toFixed(2)
-    }
+        resultado = operacion.toFixed(3)
     return resultado;
+    }
 }
-
-ecPrimerGrado(a,b)
-
-let bNeg = b*-1
-if (b>=0) {
-    ecuacion = `La ecuación es: ${a}X + ${b} = 0`
-} else {
-    ecuacion = `La ecuación es: ${a}X - ${bNeg} = 0`
-}
-
-let resultadoText = `La raíz es: ${resultado}`
 
 let ecuac = document.getElementById("ecL")
 let result = document.getElementById("resL")
 
-let botonCalc = document.getElementById("botonCalc")
-
-botonCalc.addEventListener("click", calcular)
-function calcular() {
+function validar(e) {
+    e.preventDefault()
+    let form = e.target
+    let a = form.children[0].value
+    let b = form.children[1].value
+    ecPrimerGrado(a,b)
+    let bNeg = b*-1
+    if (b>=0) {
+        ecuacion = `La ecuación es: ${a}X + ${b} = 0`
+    } else {
+        ecuacion = `La ecuación es: ${a}X - ${bNeg} = 0`
+    }
+    let resultadoText = `La raíz es: ${resultado}`
     ecuac.innerHTML = `${ecuacion}`;
     result.innerHTML = `${resultadoText}`;
+    form.reset()
 }
+
+
+
+// ----------------------- ecuación primer grado -----------------------
+
+// let coefLineal = document.getElementById("coefLineal").value
+// let a = parseInt(coefLineal)
+// let terminoInd = document.getElementById("terminoInd").value
+// let b = parseInt(terminoInd)
+
+// let operacion = 0
+// let resultado = 0
+// function ecPrimerGrado(a, b) {
+//     if (a == 0) {
+//         resultado = "inexistente"
+//     } else {
+//         operacion = (-b) / a
+//         resultado = operacion.toFixed(2)
+//     }
+//     return resultado;
+// }
+
+// ecPrimerGrado(a,b)
+
+// let bNeg = b*-1
+// if (b>=0) {
+//     ecuacion = `La ecuación es: ${a}X + ${b} = 0`
+// } else {
+//     ecuacion = `La ecuación es: ${a}X - ${bNeg} = 0`
+// }
+
+// let resultadoText = `La raíz es: ${resultado}`
+
+// let ecuac = document.getElementById("ecL")
+// let result = document.getElementById("resL")
+
+// let botonCalc = document.getElementById("botonCalc")
+
+// botonCalc.addEventListener("click", calcular)
+// function calcular() {
+//     ecuac.innerHTML = `${ecuacion}`;
+//     result.innerHTML = `${resultadoText}`;
+// }
 
 // ----------------------- ecuación segundo grado -----------------------
 
-let coefCuadC = document.getElementById("coefCuadC").value
-let aC = parseInt(coefCuadC)
-let coefLinealC = document.getElementById("coefLinealC").value
-let bC = parseInt(coefLinealC)
-let terminoIndC = document.getElementById("terminoIndC").value
-let cC = parseInt(terminoIndC)
+// let coefCuadC = document.getElementById("coefCuadC").value
+// let aC = parseInt(coefCuadC)
+// let coefLinealC = document.getElementById("coefLinealC").value
+// let bC = parseInt(coefLinealC)
+// let terminoIndC = document.getElementById("terminoIndC").value
+// let cC = parseInt(terminoIndC)
 
-let resultadoC = 0
-function ecSegundoGrado(aC, bC, cC) {
-    if (bC > 0) {
-        x_1 = (-bC - ((bC**2 - 4*aC*cC)**0.5)) / (2.0 * aC);
-        x_2 = (cC / aC) / x_1;
-        resultadoC = [x_1, x_2];
-    } else {
-        x_1 = (-bC + ((bC**2 - 4*aC*cC)**0.5)) / (2.0 * aC);
-        x_2 = (cC / aC) / x_1;
-        resultadoC = [x_1, x_2];
-    }
-    return resultadoC
-}
+// let resultadoC = 0
+// function ecSegundoGrado(aC, bC, cC) {
+//     if (bC > 0) {
+//         x_1 = (-bC - ((bC**2 - 4*aC*cC)**0.5)) / (2.0 * aC);
+//         x_2 = (cC / aC) / x_1;
+//         resultadoC = [x_1, x_2];
+//     } else {
+//         x_1 = (-bC + ((bC**2 - 4*aC*cC)**0.5)) / (2.0 * aC);
+//         x_2 = (cC / aC) / x_1;
+//         resultadoC = [x_1, x_2];
+//     }
+//     return resultadoC
+// }
 
-ecSegundoGrado(aC, bC, cC)
+// ecSegundoGrado(aC, bC, cC)
 
-let bCNeg = bC*-1
-let cCNeg = cC*-1
-if ((bC>=0) && (cC>=0)) {
-    ecuacionC = `La ecuación es: ${aC}X² + ${bC}X + ${cC}= 0`
-} else if ((bC<0) && (cC<0)) {
-    ecuacionC = `La ecuación es: ${aC}X² - ${bCNeg}X - ${cCNeg} = 0`
-} else if ((bC<0) && (cC>=0)) {
-    ecuacionC = `La ecuación es: ${aC}X² - ${bCNeg}X + ${cC} = 0`
-} else if ((bC>=0) && (cC<0)) {
-    ecuacionC = `La ecuación es: ${aC}X² + ${bC}X - ${cCNeg} = 0`
-}
+// let bCNeg = bC*-1
+// let cCNeg = cC*-1
+// if ((bC>=0) && (cC>=0)) {
+//     ecuacionC = `La ecuación es: ${aC}X² + ${bC}X + ${cC}= 0`
+// } else if ((bC<0) && (cC<0)) {
+//     ecuacionC = `La ecuación es: ${aC}X² - ${bCNeg}X - ${cCNeg} = 0`
+// } else if ((bC<0) && (cC>=0)) {
+//     ecuacionC = `La ecuación es: ${aC}X² - ${bCNeg}X + ${cC} = 0`
+// } else if ((bC>=0) && (cC<0)) {
+//     ecuacionC = `La ecuación es: ${aC}X² + ${bC}X - ${cCNeg} = 0`
+// }
 
-if ((bC**2 - 4*aC*cC) < 0) {
-    resultadoTextC = `No existe solución real`
-} else if (aC == 0) {
-    resultadoTextC = `La ecuación no es cuadrática`
-} else {
-    resultadoTextC = `Las raíces son: ${resultadoC[0].toFixed(2)} y ${resultadoC[1].toFixed(2)}`
-}
+// if ((bC**2 - 4*aC*cC) < 0) {
+//     resultadoTextC = `No existe solución real`
+// } else if (aC == 0) {
+//     resultadoTextC = `La ecuación no es cuadrática`
+// } else {
+//     resultadoTextC = `Las raíces son: ${resultadoC[0].toFixed(2)} y ${resultadoC[1].toFixed(2)}`
+// }
 
-let ecuacC = document.getElementById("ecC")
-let resultC = document.getElementById("resC")
+// let ecuacC = document.getElementById("ecC")
+// let resultC = document.getElementById("resC")
 
-let botonCalcC = document.getElementById("botonCalcC")
+// let botonCalcC = document.getElementById("botonCalcC")
 
-botonCalcC.addEventListener("click", calcularC)
-function calcularC() {
-    ecuacC.innerHTML = `${ecuacionC}`;
-    resultC.innerHTML = `${resultadoTextC}`;
-}
+// botonCalcC.addEventListener("click", calcularC)
+// function calcularC() {
+//     ecuacC.innerHTML = `${ecuacionC}`;
+//     resultC.innerHTML = `${resultadoTextC}`;
+// }
